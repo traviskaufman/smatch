989d6f2e06e84bcad4db603bc42d9a6d8f51a685
Travis Kaufman <tkaufman@appnexus.com>
2013-12-20 15:47:38 -0500

    Add grunt build stuff

---
+++ Gruntfile.js
@@ -0,0 +1,57 @@
+module.exports = function(grunt) {
+  'use strict';
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    // Traceur Build
+    traceur: {
+      options: {
+        experimental: true
+      },
+      custom: {
+        files: {
+          'lib/smatch.js': ['src/smatch.js']
+        }
+      }
+    },
+    // !jshint all code
+    jshint: {
+      options: {
+        jshintrc: true
+      },
+      all: ['Gruntfile.js', 'src/*.js', 'test/*.js']
+    },
+    // Mocha tests
+    mochaTest: {
+      options: {
+        reporter: 'list',
+        require: ['test/init'],
+        mocha: require('mocha')
+      },
+      test: ['test/*.spec.js']
+    },
+    // Production-ready uglified build
+    uglify: {
+      options: {
+        banner: '/**\n' +
+                 '* smatch - A scala-style pattern matching utility for ' +
+                 'javascript.\n' +
+                 '* @author Travis Kaufman <travis.kaufman@gmail.com>\n' +
+                 '* @copyright 2013 Travis Kaufman\n' +
+                 '* @license MIT\n' +
+                 '* @version <%= pkg.version %>\n' +
+                 '*/\n'
+      },
+      files: {
+        'dist/smatch.min.js': ['lib/smatch.js']
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-traceur');
+  grunt.loadNpmTasks('grunt-mocha-test');
+
+  grunt.registerTask('build', ['jshint', 'mochaTest', 'traceur', 'uglify']);
+};
